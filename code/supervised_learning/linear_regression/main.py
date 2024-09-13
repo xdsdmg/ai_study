9b3f01b226f7bd1018b04a816c73b9eba9c9ebb6
xdsdmg <2216336843@qq.com>
2024-09-14 07:45:07 +0800

    code(wip): linear regression

--- code/supervised_learning/linear_regression/main.py
+++ code/supervised_learning/linear_regression/main.py
@@ -1,5 +1,11 @@
 import pandas as pd
 
+
+def hypotheses(theta: list[float], x: list[float]) -> float:
+    if len(theta) != len(x):
+        raise Exception("theta and x do not have the same dimension")
+
+
 if __name__ == "__main__":
     data_file_path = "/home/zhangchi/workarea/code/ai_study/code/data/boston.csv"
     df = pd.read_csv(data_file_path)
