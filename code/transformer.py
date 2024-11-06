e5c25804831e45467484049aee92379999cbb4d0
zhangchi <2216336843@qq.com>
2024-11-07 07:50:26 +0800

    code (wip): transformer

--- code/transformer.py
+++ code/transformer.py
@@ -9,6 +9,55 @@ def get_len_mask(
     b: int, max_len: int, feat_lens: torch.Tensor, device: torch.device
 ) -> torch.Tensor:
     attn_mask = torch.ones((b, max_len, max_len), device=device)
-     
 
-    pass
+    for i in range(b):
+        attn_mask[i, :, : feat_lens[i]] = 0
+
+    return attn_mask.to(torch.bool)
+
+
+def get_subsequent_mask(b: int, max_len: int, device: torch.device) -> torch.Tensor:
+    return torch.triu(torch.ones((b, max_len, max_len), device=device), diagonal=1).to(
+        torch.bool
+    )
+
+
+def get_enc_dec_mask(
+    b: int,
+    max_feat_len: int,
+    feat_lens: torch.Tensor,
+    max_label_len: int,
+    device: torch.device,
+) -> torch.Tensor:
+    attn_mask = torch.zeros((b, max_label_len, max_feat_len), device=device)
+
+    for i in range(b):
+        attn_mask[i, :, feat_lens[i] :] = 1
+
+    return attn_mask.to(torch.bool)
+
+
+class MultiHeadAttention(nn.Module):
+    def __init__(self, d_k, d_v, d_model, num_heads, p=0.0):
+        super(MultiHeadAttention, self).__init__()
+        self.d_model = d_model
+        self.d_k = d_k
+        self.d_v = d_v
+        self.num_heads = num_heads
+        self.dropout = nn.Dropout(p)
+
+        self.W_Q = nn.Linear(d_model, d_k * num_heads)
+        self.W_K = nn.Linear(d_model, d_k * num_heads)
+        self.W_V = nn.Linear(d_model, d_v * num_heads)
+        self.W_out = nn.Linear(d_v * num_heads, d_model)
+
+        nn.init.normal_(self.W_Q.weight, mean=0, std=np.sqrt(2.0 / (d_model + d_k)))
+        nn.init.normal_(self.W_K.weight, mean=0, std=np.sqrt(2.0 / (d_model + d_k)))
+        nn.init.normal_(self.W_V.weight, mean=0, std=np.sqrt(2.0 / (d_model + d_v)))
+        nn.init.normal_(self.W_out.weight, mean=0, std=np.sqrt(2.0 / (d_model + d_v)))
+
+    def forward(self, Q, K, V, attn_mask, **kwargs):
+        N = Q.size(0)
+        q_len, k_len = Q.size(1), K.size(1)
+        d_k, d_v = self.d_k, self.d_v
+        num_heads = self.num_heads
